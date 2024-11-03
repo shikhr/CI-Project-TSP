0d517867443a97c7c41264418ede0aafac15d55f
Shikhar Mishra <shikharmishra730@gmail.com>
2024-11-03 18:25:05 +0530

    add deploy

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import TSPDemo from './components/TSPDemo';
+const App = () => {
+    return (_jsx("div", { children: _jsx(TSPDemo, {}) }));
+};
+export default App;
